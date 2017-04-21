d959d883474aeda74c6c69be9ace3aeff69a6a5f
Marc Majcher <majcher@gmail.com>
2017-04-21 10:04:33 -0500

    ascii test

--- ai-test.js
+++ ai-test.js
@@ -1,12 +1,37 @@
 'use strict';
 const art = require('ascii-art');
 
-var image = new art.Image({
-  filepath: './01_Magician.jpg',
-  alphabet: 'greyscale',
-  width: 17,
-  height: 23
+const styles = ['variant1', 'variant2', 'variant3', 'variant4',
+  'ultra-wide', 'wide', 'hatching'
+];
+
+styles.forEach(style => {
+
+  var image = new art.Image({
+    filepath: './01_Magician_bw.png',
+    alphabet: style,
+    width: 17,
+    height: 23
+  });
+
+  image.write(function(err, rendered) {
+    console.log(rendered);
+    console.log('===================');
+  });
+
 });
-image.write(function(err, rendered) {
-  console.log(rendered);
-})
+
+
+const asciify = require('asciify-image');
+
+const options = {
+  fit: 'box',
+  width: 17,
+  height: 23,
+  color: false,
+  c_ratio: 2
+};
+
+asciify('./01_Magician_bw.png', options, function(asciified) {
+  console.log(asciified);
+});
