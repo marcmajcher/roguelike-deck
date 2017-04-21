92b709ddf95ad52c63baaf0b961d8695f4d6997d
Marc Majcher <majcher@gmail.com>
2017-04-21 12:06:00 -0500

    rendering ascii

---
+++ render-ascii-cards.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const art = require('ascii-art');
+const fs = require('fs');
+
+const imgDir = 'rw-images-bw';
+const outDir = 'rw-ascii';
+const separator = '=================\n';
+const styles = ['variant1', 'variant2', 'variant3', 'variant4',
+  'ultra-wide', 'wide', 'hatching'
+];
+
+fs.readdir(imgDir, (err, files) => {
+  files.forEach(file => {
+    const outFile = `${outDir}/${file}`.replace(/\.png$/, '.txt');
+    const outStream = fs.createWriteStream(outFile);
+    outStream.write(separator);
+
+    styles.forEach(style => {
+      var image = new art.Image({
+        filepath: `${imgDir}/${file}`,
+        alphabet: style,
+        width: 17,
+        height: 23
+      });
+
+      image.write(function(err, rendered) {
+        outStream.write(rendered.replace(/\[\d+m/g, '') + separator);
+      });
+    });
+  });
+});
